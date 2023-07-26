55800e99adf57aad4faeb0beeb61f4251d9e14a5
Akinola <adewoleakinola24@gmail.com>
2023-07-26 21:48:46 +0100

    designing client-side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom'
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantDetailsPage from './routes/RestaurantDetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/restaurants/:id/update" component={UpdatePage} />
+              <Route path="/restaurants/:id" component={RestaurantDetailsPage} />
+            </Switch>
+    </Router>
     </div>
   );
 }
