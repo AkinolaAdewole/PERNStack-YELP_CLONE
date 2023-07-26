15ebefd5b52e9f544551923ebe43f8b1fbc5077f
Akinola <adewoleakinola24@gmail.com>
2023-07-26 10:33:50 +0100

    Yelp

--- Backend/db.sql
+++ Backend/db.sql
@@ -1,5 +1,20 @@
  CREATE DATABASE yelpclone;
 
  CREATE TABLE products (
-    
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    price INT,
+    on_sale BOOLEAN
+ );
+
+ ALTER TABLE products ADD COLUMN feature BOOLEAN;
+ ALTER TABLE products DROP COLUMN feature;
+
+ 
+
+ CREATE TABLE restaurants(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    location VARCHAR(100),
+    price_range INT
  );
