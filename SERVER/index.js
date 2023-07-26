16deb3b79efe40ab341df40a53f080f7f121269f
Akinola <adewoleakinola24@gmail.com>
2023-07-26 11:16:11 +0100

    Yelp

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require ("express");
 const App = express();
 
 
-App.listen(5000,()=>{
-    console.log(`Server is connected at port 5000`);
-})
+const port = 5000;
+App.listen(port,()=>{
+    console.log(`Server is connected at port ${port}`);
+});
