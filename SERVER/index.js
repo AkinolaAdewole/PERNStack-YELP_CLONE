f5dff01b511710865a1a0f07076aff5c85908ea2
Akinola <adewoleakinola24@gmail.com>
2023-07-26 12:09:58 +0100

    Yelp clone

--- SERVER/index.js
+++ SERVER/index.js
@@ -3,6 +3,15 @@ const express = require ("express");
 const App = express();
 
 
+App.get("/api/v1/restaurants",(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        data:{
+            restaurant:["MacDonalds", "Wendy"],
+        }
+    });
+});
+
 const port = process.env.PORT || 5002;
 App.listen(port,()=>{
     console.log(`Server is connected at port ${port}`);
