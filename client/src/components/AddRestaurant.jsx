b686df98527c556655b518f62c3868c5621a4122
Akinola <adewoleakinola24@gmail.com>
2023-07-26 22:10:20 +0100

    Designing Frontend

---
+++ client/src/components/AddRestaurant.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddRestaurant = () => {
+  return (
+    <div>
+         <div className='mb-4'>
+            <form action="">
+                <div className='form-row'>
+                    <div className='col'>
+                        
+                    </div>
+                </div>
+            </form>
+         </div>
+    </div>
+  )
+}
+
+export default AddRestaurant
