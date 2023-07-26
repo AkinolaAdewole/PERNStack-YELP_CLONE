3857a2047ded337ee664705f42c97b9b0b42d8a3
Akinola <adewoleakinola24@gmail.com>
2023-07-26 23:41:05 +0100

    Designing Frontend

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -7,13 +7,15 @@ const RestaurantList = () => {
         <table class="table">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Restaurant</th>
+                <th scope="col">Location</th>
+                <th scope="col">Price Range</th>
+                <th scope="col">Ratings</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
                 </tr>
             </thead>
-            
+
             <tbody>
                 <tr>
                 <th scope="row">1</th>
