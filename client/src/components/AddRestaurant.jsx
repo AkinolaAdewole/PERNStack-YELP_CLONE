9d2b77c217e0c76e070d3667490c98d40e964166
Akinola <adewoleakinola24@gmail.com>
2023-07-27 12:11:35 +0100

    sending from frontend to backend

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -10,17 +10,19 @@ const AddRestaurant = () => {
             <form action="">
                 <div className='row'>
                             <div className='col'>
-                                <input type="text" className='form-control' 
+                                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} 
+                                  className='form-control' 
                                   placeholder='Name'/>
                             </div>
 
                             <div className='col'>
                                 <input type="text" className='form-control' 
-                                  placeholder='location'/>
+                                  value={location} onChange={(e)=>setLocation(e.target.value)} placeholder='location'/>
                             </div>
 
                             <div className='col'>
-                                <select name="" id="" className='custom-select my-1 mr-sm-2'>
+                                <select value={priceRange} onChange={(e)=>setPriceRange(e.target.value)}
+                                   className='custom-select my-1 mr-sm-2'>
                                     <option disabled> Price Range</option>
                                     <option value="1">$</option>
                                     <option value="2">$$</option>
