4e52f1f56fb1fb999a6da773ded59bffbd9af0a3
Akinola <adewoleakinola24@gmail.com>
2023-07-27 22:33:03 +0100

    implementing restaurant details page and review

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 I cloned Yelp website using PostgeSQL, ExpressJs, React, NodeJs
 
 Two separate tables: Restaurant table and Review table, both have id which is the primary key
+
+I used foreign key by creating a column in the review table called Restaurant_id which is going to represent the id of the restaurant 
+that is associated with. It will allow me to create one to many relationship
--- SERVER/db.sql
+++ SERVER/db.sql
@@ -15,9 +15,12 @@
 
  CREATE TABLE reviews (
    id BIGSERIAL NOT NULL PRIMARY KEY,
+   restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
    name VARCHAR(100) NOT NULL,
    review TEXT NOT NULL,
    rating INT NOT NULL check(rating >=1 and rating <=5)
  );
 
- INSERT INTO reviews (name, review, rating) VALUES ('Carl', 'restaurant was awesome', 5);
+ INSERT INTO reviews (restaurant_id,name, review, rating) VALUES (4,'Carl', 'restaurant was awesome', 5);
+
+ SELECT * FROM reviews WHERE restaurant_id = 4;
