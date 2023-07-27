d36d34714f400b6c316d3560b46106bd816e4501
Akinola <adewoleakinola24@gmail.com>
2023-07-27 23:33:19 +0100

    implementing restaurant details page and reviews

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -4,8 +4,10 @@ const StarRating = ({rating}) => {
     const stars=[];
     for(let i=1; i<=5; i++){
         if(i<=rating){
-            stars.push(<i class="fa-solid fa-star"></i>)
-        } else{
+            stars.push(<i class="fa-solid fa-star"></i>);
+        } else if(i==Math.ceil(rating) && !Number.isInteger(rating)){
+            stars.push(<i class="fa-regular fa-star-half-stroke"></i>);
+        } else {
             stars.push(<i class="fa-regular fa-star"></i>);
         }
     }
