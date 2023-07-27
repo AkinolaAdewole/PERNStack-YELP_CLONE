2725111b2531cacfff12e3322c24c2bba70acc6d
Akinola <adewoleakinola24@gmail.com>
2023-07-27 16:05:31 +0100

    update the backend from the frontend

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const UpdateRestaurant = () => {
-    const param =useParams();
+const UpdateRestaurant = (props) => {
+    const {id} =useParams();
   return (
-    <div>UpdateRestaurant</div>
+    <div>
+         <form className='form-group'></form>
+    </div>
   )
 }
 
