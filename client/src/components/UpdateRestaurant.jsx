9100ec0dfa9098bd361a1be0d408c640dd541856
Akinola <adewoleakinola24@gmail.com>
2023-07-27 17:40:08 +0100

    updating the backend from the frontend

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { RestaurantContext } from '../ContextApi/RestaurantContextProvider';
+import RestaurantFinder from '../Api/RestaurantFinder';
 
 const UpdateRestaurant = (props) => {
-    const {id} =useParams();
+    const { id } = useParams();
+    const { restaurants } = useContext(RestaurantContext);
 
-    const[name,setName]=useState();
-    const[location,setlocation]=useState();
-    const[priceRange,setPriceRange]=useState();
+    const [name, setName] = useState("");
+    const [location, setlocation] = useState("");
+    const [priceRange, setPriceRange] = useState("");
 
-    const handleUpdate=()=>{}
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await RestaurantFinder.get(`/${id}`);
+                console.log(response);
+                const restaurantData = response.data.data.restaurant;
+                setName(restaurantData.name);
+                setlocation(restaurantData.location);
+                setPriceRange(restaurantData.price_range);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleUpdate = () => {}
 
   return (
     <div>
+        {/* <h1>{Restaurant[0].name}</h1> */}
+        <h1>{restaurants && restaurants.length > 0 ? restaurants[0].name : ""}</h1>
+
+
          <form>
             <div className='form-group mb-3'>
                 <label htmlFor="name">Name</label>
