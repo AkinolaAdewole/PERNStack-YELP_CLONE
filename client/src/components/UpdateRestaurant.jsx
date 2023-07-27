f1feaca36309f301eac278eea3fe01c00a8d24d7
Akinola <adewoleakinola24@gmail.com>
2023-07-27 16:17:55 +0100

    updating the backend from the frontend

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -5,7 +5,22 @@ const UpdateRestaurant = (props) => {
     const {id} =useParams();
   return (
     <div>
-         <form className='form-group'></form>
+         <form>
+            <div className='form-group'>
+                <label htmlFor="name">Name</label>
+                <input type="text" className='form-control' placeholder='Name' />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="name">Location</label>
+                <input type="text" className='form-control' placeholder='Location' />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="name">Price Range</label>
+                <input type="text" placeholder='Price range' className='form-control' />
+            </div>
+         </form>
     </div>
   )
 }
