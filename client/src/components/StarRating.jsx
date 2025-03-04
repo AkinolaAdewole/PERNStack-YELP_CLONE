631ae8b9d1835a1d81ac0f1a3ee3d45a2dfc592e
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-04 23:43:24 +0100

    StarRating component with these optimizations

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from 'react'
 
-const StarRating = ({ rating }) => {
-  const stars = [];
-
-  for (let i = 1; i <= 5; i++) {
-    if (i <= rating) {
-      stars.push(<i key={i} className="fa-solid fa-star"></i>);
-    } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
-      stars.push(<i key={i} className="fa-regular fa-star-half-stroke"></i>);
-    } else {
-      stars.push(<i key={i} className="fa-regular fa-star"></i>);
+const StarRating = ({rating}) => {
+    const stars=[];
+    for(let i=1; i<=5; i++){
+        if(i<=rating){
+            stars.push(<i class="fa-solid fa-star"></i>);
+        } 
+        else if(i==Math.ceil(rating) && !Number.isInteger(rating)){
+            stars.push(<i class="fa-regular fa-star-half-stroke"></i>);
+        } 
+        else {
+            stars.push(<i class="fa-regular fa-star"></i>);
+        }
     }
-  }
 
-  return <div>{stars}</div>;
-};
+    // const stars = Array.from({ length: 5 }).map((_, index) => (
+    //     <FontAwesomeIcon key={index} icon={index < rating ? faStar : ["far", "star"]} />
+    //   ));
 
-export default StarRating;
+    // const stars = [...Array(maxRating)].map((_, index) => (
+    //     <FontAwesomeIcon key={index} icon={index < rating ? solidStar : regularStar} />
+    //   ));
+
+  return (
+    <>
+      <div>{stars}</div>
+    </>
+  )
+}
+
+export default StarRating
